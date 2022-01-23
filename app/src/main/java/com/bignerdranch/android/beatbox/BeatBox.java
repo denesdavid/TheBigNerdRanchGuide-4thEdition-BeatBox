e9d0787e20fbc77499cf12aeec726f1aa03c70dc
Darius Davedane <denesd93@gmail.com>
2022-01-23 23:22:11 +0100

    Working on Chapter 20 - Test Dependencies section finished.

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -1,39 +1,59 @@
 package com.bignerdranch.android.beatbox;
 
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.SoundPool;
 import android.util.Log;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 
 public class BeatBox {
 
     private  final  String TAG = "BeatBox";
     private final String SOUNDS_FOLDER = "sample_sounds";
-    private AssetManager currentAssetManager;
+    private AssetManager assets;
+    private final int MAX_SOUNDS = 5;
+    private SoundPool soundPool = new SoundPool.Builder().setMaxStreams(MAX_SOUNDS).build();
     ArrayList<Sound> sounds;
 
+
     public BeatBox (AssetManager assetManager) {
-        currentAssetManager = assetManager;
+        assets = assetManager;
         sounds = loadSounds();
+    }
+
+    void play(Sound sound){
+        if (sound.soundId != null){
+            soundPool.play(sound.soundId, 1.0f,1.0f, 1, 0, 1.0f);
+        }
     }
 
     private ArrayList<Sound> loadSounds() {
         ArrayList<Sound> sounds = new ArrayList<>();
         try {
-            String[] soundNames = currentAssetManager.list(SOUNDS_FOLDER);
+            String[] soundNames = assets.list(SOUNDS_FOLDER);
             for (String soundName : soundNames) {
                 String assetPath = SOUNDS_FOLDER+"/"+soundName;
                 Sound sound = new Sound(assetPath);
-                sounds.add(sound);
+                //sounds.add(sound);
+                try {
+                    load(sound);
+                    sounds.add(sound);
+                } catch (Exception ex) {
+                    Log.e(TAG, "Cound not load sound $filename", ex);
+                }
             }
             return sounds;
         } catch (Exception ex) {
             Log.e(TAG, "Could not list assets", ex);
         }
         return sounds;
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = assets.openFd(sound.assetPath);
+        Integer soundId = soundPool.load(afd, 1);
+        sound.soundId = soundId;
     }
 }
--- app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -5,9 +5,12 @@ import java.lang.reflect.Array;
 public class Sound {
 
     private final String WAV = ".wav";
+    public String assetPath;
     public String name;
+    public Integer soundId = null;
 
     public Sound (String assetPath) {
+        this.assetPath = assetPath;
         String[] splittedPath = assetPath.split("/");
         String nameWithExtension = splittedPath[splittedPath.length-1];
         String nameWithoutExtension = nameWithExtension.
